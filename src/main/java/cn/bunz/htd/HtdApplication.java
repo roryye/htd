707c82840d86824d065eb4383617444e5d688694
YeWeiqiang <yeweiqiang002@gmail.com>
2019-10-19 21:35:03 +0800

    first commit.

---
+++ src/main/java/cn/bunz/htd/HtdApplication.java
@@ -0,0 +1,13 @@
+package cn.bunz.htd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bunz/htd/HtdApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bunz.htd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
